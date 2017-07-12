2e2069b5a017644d01412aadb67ba2e1f3332b23
Iain <iainmacdonald16@gmail.com>
2017-07-12 14:18:53 +0100

    Added comments to the robot scripts

--- robot.js
+++ robot.js
@@ -4,41 +4,72 @@ const { Server } = require('http');
 const socketIO = require('socket.io');
 const songs = require('j5-songs');
 
+// Import project config
 const config = require('./config');
 
+// Set up the socker server
 const app = express();
 const http = Server(app);
 const io = socketIO.listen(http);
 
+// Make a new johnny-five Board() instance
 const board = new Board();
 
+// Begin the server under the specified port
 http.listen(config.port, () => {
   console.log('Server Running');
 });
 
+/**
+ *
+ * When the board is ready, notify the terminal console.
+ * Then specify the buzzer's pin using the Piezo class.
+ *
+ */
 board.on('ready', function() {
-
   console.log('board ready');
 
+  // Store the Piezo in a constant
   const buzzer = new Piezo(3);
 
   io.on('connect', (client) => {
 
     client.on('join', handshake => {
+      /**
+       *
+       * Let the app know that they've now connected to the robot so the app's
+       * interface can be updated to remove the loading animation and show the
+       * playlist controls.
+       *
+       */
       io.emit('robot-connected', 'Robot Connected');
-
+      // Write the handshake in the terminal console
       console.log(handshake);
     });
 
+    /**
+     *
+     * When the app selects a song to play, stop the buzzer playing the current
+     * song, then play the selected song.
+     *
+     */
     client.on('play-song', (song) => {
       buzzer.stop();
-      // buzzer.off();
-      buzzer.play(songs.load(song));
+      console.log(`Current song is ${song}`);
+      buzzer.play(songs.load(song), (songEnded) => {
+        if(songEnded) {
+          io.emit('song-ended');
+        }
+      });
     });
 
+    /**
+     *
+     * If the app selects a song that's already playing, stop the buzzer.
+     *
+     */
     client.on('stop-song', () => {
       buzzer.stop();
-      buzzer.off();
     });
 
   });
